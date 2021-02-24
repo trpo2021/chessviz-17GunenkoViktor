6ab18435e62130a027f1d3947c7c15c4010deb1d
Gunenko Viktor <17GunenkoViktor@users.noreply.github.com>
2021-02-24 02:25:34 -0500

    Add ches coord

--- chessviz.c
+++ chessviz.c
@@ -2,50 +2,67 @@
 
 int main()
 {
-    char Desc[8][8];
+    char Desc[9][9];
     int i,k;
-    Desc[0][0]='r';
-    Desc[0][1]='n';
+    Desc[1][1]='r';
+    Desc[1][2]='n';
+    Desc[1][3]='b';
+    Desc[1][4]='q';
+    Desc[1][5]='k';
+    Desc[1][6]='b';
+    Desc[1][7]='n';
+    Desc[1][8]='r';
+    Desc[2][1]='p';
+    Desc[2][2]='p';
+    Desc[2][3]='p';
+    Desc[2][4]='p';
+    Desc[2][5]='p';
+    Desc[2][6]='p';
+    Desc[2][7]='p';
+    Desc[2][8]='p';
+    Desc[7][1]='P';
+    Desc[7][2]='P';
+    Desc[7][3]='P';
+    Desc[7][4]='P';
+    Desc[7][5]='P';
+    Desc[7][6]='P';
+    Desc[7][7]='P';
+    Desc[7][8]='P';
+    Desc[8][1]='R';
+    Desc[8][2]='N';
+    Desc[8][3]='B';
+    Desc[8][4]='Q';
+    Desc[8][5]='K';
+    Desc[8][6]='B';
+    Desc[8][7]='N';
+    Desc[8][8]='R';
+    Desc[0][1]='a';
     Desc[0][2]='b';
-    Desc[0][3]='q';
-    Desc[0][4]='k';
-    Desc[0][5]='b';
-    Desc[0][6]='n';
-    Desc[0][7]='r';
-    Desc[1][0]='p';
-    Desc[1][1]='p';
-    Desc[1][2]='p';
-    Desc[1][3]='p';
-    Desc[1][4]='p';
-    Desc[1][5]='p';
-    Desc[1][6]='p';
-    Desc[1][7]='p';
-    Desc[6][0]='P';
-    Desc[6][1]='P';
-    Desc[6][2]='P';
-    Desc[6][3]='P';
-    Desc[6][4]='P';
-    Desc[6][5]='P';
-    Desc[6][6]='P';
-    Desc[6][7]='P';
-    Desc[7][0]='R';
-    Desc[7][1]='N';
-    Desc[7][2]='B';
-    Desc[7][3]='Q';
-    Desc[7][4]='K';
-    Desc[7][5]='B';
-    Desc[7][6]='N';
-    Desc[7][7]='R';
-    for (i=2;i<6;i++)
+    Desc[0][3]='c';
+    Desc[0][4]='d';
+    Desc[0][5]='e';
+    Desc[0][6]='f';
+    Desc[0][7]='g';
+    Desc[0][8]='h';
+    Desc[0][0]=' ';
+    Desc[1][0]='1';
+    Desc[2][0]='2';
+    Desc[3][0]='3';
+    Desc[4][0]='4';
+    Desc[5][0]='5';
+    Desc[6][0]='6';
+    Desc[7][0]='7';
+    Desc[8][0]='8';
+    for (i=3;i<7;i++)
     {
-        for (k=0;k<8;k++)
+        for (k=1;k<9;k++)
         {
             Desc[i][k]=' ';
         }
     }
-    for (i=0;i<8;i++)
+    for (i=0;i<9;i++)
     {
-        for(k=0;k<8;k++)
+        for(k=0;k<9;k++)
         {
             printf("%c ",Desc[i][k]);
         }
