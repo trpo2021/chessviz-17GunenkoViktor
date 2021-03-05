f1220db8d7f7c3ab127532fc26787a311d82ddbc
17GunenkoViktor <79318055+17GunenkoViktor@users.noreply.github.com>
2021-03-05 14:54:35 +0700

    add Pp-move

--- chessviz.c
+++ chessviz.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
-
+#include <string.h>
+void Print(char Desc[9][9]){
+	int i,k;
+	for (i=0;i<9;i++)
+    {
+        for(k=0;k<9;k++)
+        {
+            printf("%c ",Desc[i][k]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
 int main()
 {
     char Desc[9][9];
-    int i,k;
+    char move[7];
+	int i,k,cori,corj,corI,corJ;//i- string j-colum 
     Desc[1][1]='r';
     Desc[1][2]='n';
     Desc[1][3]='b';
@@ -45,14 +58,14 @@ int main()
     Desc[0][7]='g';
     Desc[0][8]='h';
     Desc[0][0]=' ';
-    Desc[1][0]='1';
-    Desc[2][0]='2';
-    Desc[3][0]='3';
-    Desc[4][0]='4';
-    Desc[5][0]='5';
-    Desc[6][0]='6';
-    Desc[7][0]='7';
-    Desc[8][0]='8';
+    Desc[1][0]='8';
+    Desc[2][0]='7';
+    Desc[3][0]='6';
+    Desc[4][0]='5';
+    Desc[5][0]='4';
+    Desc[6][0]='3';
+    Desc[7][0]='2';
+    Desc[8][0]='1';
     for (i=3;i<7;i++)
     {
         for (k=1;k<9;k++)
@@ -60,13 +73,120 @@ int main()
             Desc[i][k]=' ';
         }
     }
-    for (i=0;i<9;i++)
-    {
-        for(k=0;k<9;k++)
-        {
-            printf("%c ",Desc[i][k]);
-        }
-        printf("\n");
-    }
+    move[0]='6';
+    Print(Desc);
+    while (move[0]!='0'){
+    	scanf ("%s",move);
+    	if(move[0]=='0'){
+    		return 0;
+		}
+    	switch(move[0]){
+    		case 'a':
+    			corj=1;
+    			break;
+    		case 'b':
+    			corj=2;
+    			break;
+    		case 'c':
+    			corj=3;
+    			break;
+    		case 'd':
+    			corj=4;
+    			break;
+    		case 'e':
+    			corj=5;
+    			break;
+    		case 'f':
+    			corj=6;
+    			break;
+    		case 'g':
+    			corj=7;
+    			break;
+    		case 'h':
+    			corj=8;
+    			break;
+    	}
+    	switch(move[1]){
+    		case '1':
+    			cori=8;
+    			break;
+    		case '2':
+    			cori=7;
+    			break;
+    		case '3':
+    			cori=6;
+    			break;
+    		case '4':
+    			cori=5;
+    			break;
+    		case '5':
+    			cori=4;
+    			break;
+    		case '6':
+    			cori=3;
+    			break;
+    		case '7':
+    			cori=2;
+    			break;
+    		case '8':
+    			cori=1;
+    			break;
+		}
+		switch(move[3]){
+    		case 'a':
+    			corJ=1;
+    			break;
+    		case 'b':
+    			corJ=2;
+    			break;
+    		case 'c':
+    			corJ=3;
+    			break;
+    		case 'd':
+    			corJ=4;
+    			break;
+    		case 'e':
+    			corJ=5;
+    			break;
+    		case 'f':
+    			corJ=6;
+    			break;
+    		case 'g':
+    			corJ=7;
+    			break;
+    		case 'h':
+    			corJ=8;
+    			break;
+		}
+		switch(move[4]){
+    		case '1':
+    			corI=8;
+    			break;
+    		case '2':
+    			corI=7;
+    			break;
+    		case '3':
+    			corI=6;
+    			break;
+    		case '4':
+    			corI=5;
+    			break;
+    		case '5':
+    			corI=4;
+    			break;
+    		case '6':
+    			corI=3;
+    			break;
+    		case '7':
+    			corI=2;
+    			break;
+    		case '8':
+    			corI=1;
+    			break;
+		}
+		Desc[corI][corJ]=Desc[cori][corj];
+		Desc[cori][corj]=' ';
+		Print(Desc);
+	}
     return 0;
 }
